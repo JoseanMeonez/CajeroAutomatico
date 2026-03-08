d67038801556c38ce2eb1829098702d4669e18fe
Andrés Meoñez <76024612+JoseanMeonez@users.noreply.github.com>
2026-03-08 01:33:51 -0600

    feat: Añadir clase CargaDatos para cargar clientes desde un archivo CSV

---
+++ src/main/java/hn/uth/cajeroautomatico/util/CargaDatos.java
@@ -0,0 +1,14 @@
+package hn.uth.cajeroautomatico.util;
+
+import hn.uth.cajeroautomatico.models.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CargaDatos {
+
+    public static List<Cliente> cargarClientes() {
+        // TODO: implementar lectura desde clientes.csv
+        return new ArrayList<>();
+    }
+}
